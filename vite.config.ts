fa6b45411800110633da540b2febe950c9fb9554
Chris Roth <chris@cjroth.com>
2025-02-27 18:57:23 -0500

    vite analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import solid from 'vite-plugin-solid'
 
 // @ts-expect-error process is a nodejs global
@@ -7,7 +8,13 @@ const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [tailwindcss(), solid()],
+  plugins: [
+    tailwindcss(),
+    solid(),
+    analyzer({
+      analyzerMode: 'static',
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //

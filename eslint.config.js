82d75d556e4be87091eb13df9e011e917744b067
Raí Adriano <raivieiraadriano92@gmail.com>
2025-08-30 11:03:16 -0300

    fix: update no-unused-vars rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
 import js from '@eslint/js'
 import typescript from '@typescript-eslint/eslint-plugin'
@@ -9,54 +9,71 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import prettier from 'eslint-config-prettier'
 import globals from 'globals'
 
-export default [js.configs.recommended, prettier, {
-  files: ['src/**/*.{ts,tsx}'],
-  ignores: ['node_modules', 'dist', 'dist-isolation', 'src-tauri', 'thunderbird-*', 'backend'],
-  languageOptions: {
-    parser: typescriptParser,
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['node_modules', 'dist', 'dist-isolation', 'src-tauri', 'thunderbird-*', 'backend'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: 'readonly',
+        // TypeScript types
+        NodeJS: 'readonly',
+        RequestInfo: 'readonly',
+        RequestInit: 'readonly',
       },
     },
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      React: 'readonly',
-      // TypeScript types
-      NodeJS: 'readonly',
-      RequestInfo: 'readonly',
-      RequestInit: 'readonly',
+    plugins: {
+      '@typescript-eslint': typescript,
+      react,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // TypeScript rules
+      // turn off the base rule to avoid duplicate reports
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_', // fn args like (_evt)
+          varsIgnorePattern: '^_', // variables like const _x = ...
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_', // catch ( _err ) { ... }
+          ignoreRestSiblings: true, // const { used, ..._rest } = obj
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+
+      // React rules
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // React Hooks rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // General rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
     },
   },
-  plugins: {
-    '@typescript-eslint': typescript,
-    react,
-    'react-hooks': reactHooks,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    // TypeScript rules
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-
-    // React rules
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    
-    // React Hooks rules
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-
-    // General rules
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'prefer-const': 'error',
-  },
-}, ...storybook.configs["flat/recommended"]];
+  ...storybook.configs['flat/recommended'],
+]

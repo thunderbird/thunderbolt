3213200672e35f1ab224dcc3e912deb922d127b4
Chris Roth <chris@cjroth.com>
2025-09-23 15:08:12 -0400

    production

---
+++ backend/src/cluster.ts
@@ -0,0 +1,11 @@
+import cluster from 'node:cluster'
+import os from 'node:os'
+import process from 'node:process'
+
+if (cluster.isPrimary) {
+  	for (let i = 0; i < os.availableParallelism(); i++)
+    	cluster.fork()
+} else {
+  	await import('.')
+  	console.log(`Worker ${process.pid} started`)
+}

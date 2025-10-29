0e68b88bcba3cbc7e9fca1518402d74b275311c1
Raí Adriano <raivieiraadriano92@gmail.com>
2025-10-29 15:37:16 -0300

    feat: implement ChatDataProvider to manage data query

--- src/chats/detail.tsx
+++ src/chats/detail.tsx
@@ -1,20 +1,12 @@
+import { ChatDataProvider } from './chat-data-provider'
 import ChatState from './chat-state'
-import { useChatPersistence } from './use-chat-persistence'
 
 export default function ChatDetailPage() {
-  const { chatThread, id, isLoading, messages, saveMessages } = useChatPersistence()
-
-  if (!id) {
-    return <div>No chat thread ID</div>
-  }
-
-  if (isLoading) {
-    return null
-  }
-
   return (
-    <div className="h-full w-full">
-      <ChatState key={id} chatThread={chatThread} id={id} initialMessages={messages} saveMessages={saveMessages} />
-    </div>
+    <ChatDataProvider>
+      <div className="h-full w-full">
+        <ChatState />
+      </div>
+    </ChatDataProvider>
   )
 }

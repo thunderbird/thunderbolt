9fa8a9c6aacc457e1068bd4434d509b05de0425f
Chris Roth <chris@cjroth.com>
2025-11-12 23:15:31 -0500

    Add debug logging to track jest global setup in CI

--- happydom.ts
+++ happydom.ts
@@ -24,6 +24,8 @@ if (typeof globalThis.Buffer === 'undefined') {
 // This MUST be set up before @testing-library loads to avoid race conditions
 import { install } from '@sinonjs/fake-timers'
 
+console.log('[happydom.ts] Installing fake timers...')
+
 // Install fake timers globally at module load time
 // This ensures they're always available when @testing-library checks for them
 // Note: We don't fake requestAnimationFrame because happy-dom hasn't fully initialized yet
@@ -33,9 +35,14 @@ const globalClock = install({
   toFake: ['setTimeout', 'clearTimeout', 'setInterval', 'clearInterval', 'Date'],
 })
 
+console.log('[happydom.ts] Fake timers installed, setting up jest global...')
+
 // Set up jest global with methods connected to the global clock
 const jestApi = {
-  advanceTimersByTime: (ms: number) => globalClock.tick(ms),
+  advanceTimersByTime: (ms: number) => {
+    console.log(`[jest.advanceTimersByTime] Called with ${ms}ms`)
+    return globalClock.tick(ms)
+  },
   runAllTimers: () => globalClock.runAll(),
   runOnlyPendingTimers: () => globalClock.runToLast(),
   clearAllTimers: () => globalClock.reset(),
@@ -50,6 +57,14 @@ if (typeof global !== 'undefined') {
   global.jest = jestApi
 }
 
+console.log('[happydom.ts] jest global set up:', {
+  hasGlobalThisJest: !!globalThis.jest,
+  hasGlobalJest: typeof global !== 'undefined' && !!(global as any).jest,
+  advanceTimersByTimeType: typeof globalThis.jest?.advanceTimersByTime,
+})
+
 // Export the clock so testing-library.ts can access it
 // @ts-ignore
 globalThis.__GLOBAL_FAKE_CLOCK__ = globalClock
+
+console.log('[happydom.ts] Setup complete')

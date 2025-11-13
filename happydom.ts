4773731bb5b8fe1375832ebea31869c7d43b0c84
Chris Roth <chris@cjroth.com>
2025-11-12 23:19:33 -0500

    Disable @testing-library fake timer detection
    The issue was that @testing-library/react's fake timer detection
    was finding jest.advanceTimersByTime() but the global wasn't persisting
    in CI (likely due to Bun test isolation).
    
    Solution: Disable fake timer detection entirely by defining
    jestFakeTimersAreEnabled() to always return false. We manage our
    own fake timers in beforeEach/afterEach instead.
    
    This way @testing-library won't try to use jest timers at all.

--- happydom.ts
+++ happydom.ts
@@ -20,51 +20,5 @@ if (typeof globalThis.Buffer === 'undefined') {
   globalThis.Buffer = Buffer
 }
 
-// Set up jest global with fake timer support IMMEDIATELY
-// This MUST be set up before @testing-library loads to avoid race conditions
-import { install } from '@sinonjs/fake-timers'
-
-console.log('[happydom.ts] Installing fake timers...')
-
-// Install fake timers globally at module load time
-// This ensures they're always available when @testing-library checks for them
-// Note: We don't fake requestAnimationFrame because happy-dom hasn't fully initialized yet
-const globalClock = install({
-  now: Date.now(),
-  shouldAdvanceTime: false,
-  toFake: ['setTimeout', 'clearTimeout', 'setInterval', 'clearInterval', 'Date'],
-})
-
-console.log('[happydom.ts] Fake timers installed, setting up jest global...')
-
-// Set up jest global with methods connected to the global clock
-const jestApi = {
-  advanceTimersByTime: (ms: number) => {
-    console.log(`[jest.advanceTimersByTime] Called with ${ms}ms`)
-    return globalClock.tick(ms)
-  },
-  runAllTimers: () => globalClock.runAll(),
-  runOnlyPendingTimers: () => globalClock.runToLast(),
-  clearAllTimers: () => globalClock.reset(),
-  getTimerCount: () => globalClock.countTimers(),
-}
-
-// @ts-ignore - Set on globalThis
-globalThis.jest = jestApi
-// @ts-ignore - Also set on global for compatibility
-if (typeof global !== 'undefined') {
-  // @ts-ignore
-  global.jest = jestApi
-}
-
-console.log('[happydom.ts] jest global set up:', {
-  hasGlobalThisJest: !!globalThis.jest,
-  hasGlobalJest: typeof global !== 'undefined' && !!(global as any).jest,
-  advanceTimersByTimeType: typeof globalThis.jest?.advanceTimersByTime,
-})
-
-// Export the clock so testing-library.ts can access it
-// @ts-ignore
-globalThis.__GLOBAL_FAKE_CLOCK__ = globalClock
-
-console.log('[happydom.ts] Setup complete')
+// Fake timers are now managed in testing-library.ts
+// This file just sets up the DOM environment

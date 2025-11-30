1644b7a17e4046fca4f8d4f737b129fc9f4b0acd
Raí Adriano <raivieiraadriano92@gmail.com>
2025-11-30 15:15:06 -0300

    feat: integrate Suspense for improved lazy loading in App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { AppErrorScreen } from './components/app-error-screen'
 import { useAppInitialization } from './hooks/use-app-initialization'
 import Loading from './loading'
@@ -18,7 +18,11 @@ export const App = () => {
       return <Loading />
     }
 
-    return <AppContent initData={initData} />
+    return (
+      <Suspense>
+        <AppContent initData={initData} />
+      </Suspense>
+    )
   }
 
   return (

8ff68d8e0f886644f41493a20f3fe83535aec973
Raí Adriano <raivieiraadriano92@gmail.com>
2026-03-03 13:36:26 -0300

    test: button test

--- src/layout/sidebar/chat-sidebar.tsx
+++ src/layout/sidebar/chat-sidebar.tsx
@@ -16,6 +16,7 @@ import { ChatList } from './chat-list'
 import { NavigationMenu } from './navigation-menu'
 import { SidebarHeader } from './sidebar-header'
 import type { ChatThread } from './types'
+import { Button } from '@/components/ui/button'
 
 type ChatSidebarContentProps = {
   isMobile: boolean
@@ -66,7 +67,7 @@ export const ChatSidebarContent = ({
   return (
     <SidebarContent className="flex flex-col h-full overflow-hidden">
       <SidebarHeader onToggle={toggleSidebar} />
-
+      <Button enableHaptics>Test</Button>
       <SidebarGroup className="flex-shrink-0">
         <SidebarGroupContent>
           <SidebarMenu>

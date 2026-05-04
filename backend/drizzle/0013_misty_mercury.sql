013e874eb0e7fca3db235e187bbaf117c91b08ce
Ítalo Menezes <italo.menezes@gmail.com>
2026-05-04 17:35:34 -0300

    feat: add jwks table for Better Auth JWT plugin signing keys

---
+++ backend/drizzle/0013_misty_mercury.sql
@@ -0,0 +1,11 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+CREATE TABLE "jwks" (
+	"id" text PRIMARY KEY NOT NULL,
+	"public_key" text NOT NULL,
+	"private_key" text NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"expires_at" timestamp
+);

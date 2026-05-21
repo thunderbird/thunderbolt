9d25080731d5b240234aed97b490bfb91c0a6647
Ítalo Menezes <italo.menezes@gmail.com>
2026-05-20 22:35:11 -0300

    refactor: drop isPrPreview from isWaitlistBypassed helper

--- src/lib/auth-mode.ts
+++ src/lib/auth-mode.ts
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { isPrPreview } from './platform'
-
 export const isSsoMode = () => import.meta.env.VITE_AUTH_MODE === 'sso'
 
 /**
@@ -15,4 +13,4 @@ export const isAnonymousAuthEnabled = () => import.meta.env.VITE_AUTH_ENABLE_ANO
 /**
  * Returns true when the waitlist gate is bypassed for this deployment
  */
-export const isWaitlistBypassed = () => import.meta.env.VITE_BYPASS_WAITLIST === 'true' || isPrPreview()
+export const isWaitlistBypassed = () => import.meta.env.VITE_BYPASS_WAITLIST === 'true'
